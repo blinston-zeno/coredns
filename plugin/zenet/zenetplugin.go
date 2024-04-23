0cb5fb42553ec170e41ca7eb8551c71c1b2a268f
Blinston S. Fernandes <blinston@zenomoto.com>
2024-04-23 11:53:36 +0530

    zenet cleanup

--- plugin/zenet/zenetplugin.go
+++ plugin/zenet/zenetplugin.go
@@ -1,91 +1,121 @@
 package zenet
 
 import (
-    "context"
-    "fmt"
-    "net"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net"
 
-    "github.com/coredns/coredns/plugin"
-    "github.com/coredns/coredns/request"
-    "github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/core/dnsserver"
+	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/request"
 
-    "github.com/coredns/caddy"
-    "github.com/miekg/dns"
-    "go.nanomsg.org/mangos/v3"
-    "go.nanomsg.org/mangos/v3/protocol/req"
-    _ "go.nanomsg.org/mangos/v3/transport/tcp"
+	"github.com/coredns/caddy"
+	"github.com/miekg/dns"
+	"go.nanomsg.org/mangos/v3"
+	"go.nanomsg.org/mangos/v3/protocol/req"
+	_ "go.nanomsg.org/mangos/v3/transport/tcp"
 )
 
 type ZenetPlugin struct {
-    Next plugin.Handler
-    Addr string      // Address of the Mangos service
-    Sock mangos.Socket
+	Next plugin.Handler
+	Addr string // Address of the Mangos service
+	Sock mangos.Socket
+}
+
+type ResolverQuery struct {
+	ResolverQuery struct {
+		Name string `json:"name"`
+	} `json:"query"`
+}
+
+type Message struct {
+	Query string `json:"query"`
 }
 
 func (zp *ZenetPlugin) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
-    state := request.Request{W: w, Req: r}
+	state := request.Request{W: w, Req: r}
 
-    if err := zp.Sock.Send([]byte(state.Name())); err != nil {
-        return dns.RcodeServerFailure, fmt.Errorf("failed to send query to mangos service: %v", err)
-    }
+	queryData := ResolverQuery{
+		ResolverQuery: struct {
+			Name string `json:"name"`
+		}{Name: state.Name()},
+	}
 
-    msg, err := zp.Sock.Recv()
-    if err != nil {
-        return dns.RcodeServerFailure, fmt.Errorf("failed to receive reply from mangos service: %v", err)
-    }
+	jsonData, err := json.Marshal(queryData)
+	if err != nil {
+		return dns.RcodeServerFailure, fmt.Errorf("failed to encode query as JSON: %v", err)
+	}
 
-    ip := net.ParseIP(string(msg))
-    msgResp := new(dns.Msg)
-    msgResp.SetReply(r)
-    msgResp.Authoritative = true
+	fmt.Println(string(jsonData))
 
-    if ip.To4() != nil {
-        aRecord := &dns.A{
-            Hdr: dns.RR_Header{Name: state.Name(), Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 3600},
-            A:   ip,
-        }
-        msgResp.Answer = append(msgResp.Answer, aRecord)
-    } else if ip.To16() != nil {
-        aaaaRecord := &dns.AAAA{
-            Hdr:  dns.RR_Header{Name: state.Name(), Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 3600},
-            AAAA: ip,
-        }
-        msgResp.Answer = append(msgResp.Answer, aaaaRecord)
-    } else {
-        // If the IP is neither IPv4 nor IPv6, return an error or handle appropriately.
-        return dns.RcodeServerFailure, fmt.Errorf("invalid IP address format: %s", string(msg))
-    }
+	if err := zp.Sock.Send(jsonData); err != nil {
+		return dns.RcodeServerFailure, fmt.Errorf("failed to send query to mangos service: %v", err)
+	}
 
-    w.WriteMsg(msgResp)
+	msg, err := zp.Sock.Recv()
+	if err != nil {
+		return dns.RcodeServerFailure, fmt.Errorf("failed to receive reply from mangos service: %v", err)
+	}
 
-    return dns.RcodeSuccess, nil
+	var msgData Message
+	err = json.Unmarshal(msg, &msgData)
+	if err != nil {
+		return dns.RcodeServerFailure, fmt.Errorf("failed to decode JSON message: %v", err)
+	}
+
+	ip := net.ParseIP(msgData.Query)
+	msgResp := new(dns.Msg)
+	msgResp.SetReply(r)
+	msgResp.Authoritative = true
+
+	if ip.To4() != nil {
+		aRecord := &dns.A{
+			Hdr: dns.RR_Header{Name: state.Name(), Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 3600},
+			A:   ip,
+		}
+		msgResp.Answer = append(msgResp.Answer, aRecord)
+	} else if ip.To16() != nil {
+		aaaaRecord := &dns.AAAA{
+			Hdr:  dns.RR_Header{Name: state.Name(), Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 3600},
+			AAAA: ip,
+		}
+		msgResp.Answer = append(msgResp.Answer, aaaaRecord)
+	} else {
+		// If the IP is neither IPv4 nor IPv6, return an error or handle appropriately.
+		return dns.RcodeServerFailure, fmt.Errorf("invalid IP address format: %s", string(msg))
+	}
+
+	w.WriteMsg(msgResp)
+
+	return dns.RcodeSuccess, nil
 }
 
 func (zp *ZenetPlugin) Name() string { return "zenet" }
 
 func setup(c *caddy.Controller) error {
-    addr := "tcp://localhost:40899"
+	addr := "tcp://localhost:40899"
 
-    sock, err := req.NewSocket()
-    if err != nil {
-        return fmt.Errorf("failed to create mangos socket: %v", err)
-    }
-    if err := sock.Dial(addr); err != nil {
-        return fmt.Errorf("failed to dial mangos service: %v", err)
-    }
+	sock, err := req.NewSocket()
+	if err != nil {
+		return fmt.Errorf("failed to create mangos socket: %v", err)
+	}
+	if err := sock.Dial(addr); err != nil {
+		return fmt.Errorf("failed to dial mangos service: %v", err)
+	}
 
-    c.Next()
-    if c.NextArg() {
-        return plugin.Error("zenet", c.ArgErr())
-    }
+	c.Next()
+	if c.NextArg() {
+		return plugin.Error("zenet", c.ArgErr())
+	}
 
-    dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
-        return &ZenetPlugin{Next: next, Addr: addr, Sock: sock}
-    })
+	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
+		return &ZenetPlugin{Next: next, Addr: addr, Sock: sock}
+	})
 
-    return nil
+	return nil
 }
 
 func init() {
-    plugin.Register("zenet", setup)
+	plugin.Register("zenet", setup)
 }
